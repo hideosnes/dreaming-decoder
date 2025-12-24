c86c6150139e187642be127cd6ce3ced9c40a0ab
Hidéo Snes <wave@hideosnes.online>
2025-12-24 15:29:32 +0100

    PB config for Dokploy

--- src/lib/pocketbase/pocketbaseClient.ts
+++ src/lib/pocketbase/pocketbaseClient.ts
@@ -4,7 +4,7 @@ export function createPB() {
     const url = import.meta.env?.POCKETBASE_URL ||
                 import.meta.env?.DEV
                     ? 'http://127.0.0.1:8090'
-                    : 'http://pocketbase:8090'
+                    : 'http://dreaming-decoder-pocketbase-tedji6:8090'
     return new PocketBase(url)
 }
 
