d958ccbb550ccbde6b6ac843b5be50235c4f04c9
Hidéo Snes <wave@hideosnes.online>
2025-12-24 15:43:29 +0100

    PB config for Dokploy

--- src/lib/pocketbase/pocketbaseClient.ts
+++ src/lib/pocketbase/pocketbaseClient.ts
@@ -1,11 +1,14 @@
 import PocketBase from "pocketbase"
 
 export function createPB() {
-    const url = import.meta.env?.POCKETBASE_URL ||
-                import.meta.env?.DEV
-                    ? 'http://127.0.0.1:8090'
-                    : 'http://dreaming-decoder-pocketbase-tedji6:8090'
-    return new PocketBase(url)
+    
+    // server-side: use Dokploy service 
+    if (typeof window === 'undefined') {
+        return new PocketBase(import.meta.env?.POCKETBASE_URL)
+    }
+
+    // client-side: use public pb-api via HTTPS
+    return new PocketBase('https://pb.app.homahuki.eu')
 }
 
 const pb = createPB()
