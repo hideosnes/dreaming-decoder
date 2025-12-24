ff9ed159fa014443d4ac37d0dec56312bb9019df
Hidéo Snes <wave@hideosnes.online>
2025-12-24 14:44:33 +0100

    PB config for Dokploy

--- src/lib/pocketbase/pocketbaseClient.ts
+++ src/lib/pocketbase/pocketbaseClient.ts
@@ -1,5 +1,12 @@
 import PocketBase from "pocketbase"
 
-const pb = new PocketBase ("http://127.0.0.1:8090")
+export function createPB() {
+    const url = import.meta.env?.POCKETBASE_URL ||
+                import.meta.env?.DEV
+                    ? 'http://127.0.0.1:8090'
+                    : 'http://pocketbase:8090'
+    return new PocketBase(url)
+}
 
+const pb = createPB()
 export default pb
